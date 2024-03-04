012dd1d79f8bf97c61c0af7daacb2db6082ceae4
kirill khlebov <khlebov79@gmail.com>
2024-03-04 17:11:26 +0300

    init: footer

--- src/js/catalog-button.js
+++ src/js/catalog-button.js
@@ -1,12 +1,17 @@
 const catalogButtonElement = document.querySelector('.navigation-menu__catalog')
 const catalogButtonImageElement = document.querySelector('.navigation-menu__icon')
 const catalogButtonTextElement = document.querySelector('.navigation-menu__text')
+const catalogListElement = document.querySelector('.navigation-menu__catalog-list')
 
 catalogButtonElement.addEventListener('click', () => {
   catalogButtonImageElement.classList.toggle('catalog-active')
+  catalogButtonElement.classList.toggle('button-active')
+  catalogListElement.classList.toggle('list-active')
   if(catalogButtonImageElement.classList.contains('catalog-active')) {
     catalogButtonTextElement.style.color = '#fff'
   } if(!catalogButtonImageElement.classList.contains('catalog-active')) {
     catalogButtonTextElement.style.color = '#2d3440'
   }
+
+
 })
